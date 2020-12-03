e1843108c5412cb5cff52df043cebdf9b512076c
Gobans <audghks4452@gmail.com>
2020-12-03 23:34:11 +0900

    product display in popup.html

--- popup.js
+++ popup.js
@@ -5,18 +5,28 @@ let tempCount = 0
 function onWindowLoad() {
         (async function fetchPopup(){
             let count = await getCount()
-            console.log(count)
             let sources = await getSources()
             console.log(sources)
             if (count != tempCount){
-                let categories =document.querySelectorAll('.category')
-                for(let index = tempCount; tempCount < count; tempCount ++){
-                    let existCategory, parentCategory = await getCategory(index,categories,sources) // 원래 있는 카테고리 인지 확인
+                console.log(count)
+                console.log(tempCount)
+                for(let index = tempCount; index < count; index ++){
+                    let categories =document.querySelectorAll('.category')
+                    console.log(categories)
+                    let [existCategory, parentCategory] = await getCategory(index,categories,sources) // 원래 있는 카테고리 인지 확인
+                    console.log(existCategory)
                     if (existCategory){
-                        
+                        let product = await sortAppend(index,sources)
+                        product.firstChild.innerText = ""
+                        parentCategory.parentNode.append(product)
+                        //카테고리는 따로 뺴서 추가해야 할 것 같다.
                     }else{
-                        let container = await sortAppend(index,sources)
+                        let product = await sortAppend(index,sources)
+                        let container = document.createElement('div')
+                        container.classList.add('container')
+                        container.appendChild(product)
                         document.body.append(container)
+                        tempCount =count
                     }
                 }
             }
@@ -53,19 +63,21 @@ async function getCategory(index,categories,sources){
     let existCategory = false
     let parentCategory = null
     for(let i in categories){
+        console.log(categories[i])
         if (categories[i].innerText == sources.sources[index].category){
             parentCategory = categories[i].parentNode
             existCategory = true
             break
         }
     }
-    return existCategory,parentCategory
+    console.log(existCategory)
+    return [existCategory,parentCategory]
 }
 
  async function sortAppend(i,sources){
 
-        let container = document.createElement('div')
-        container.classList.add('container')
+        let product = document.createElement('div')
+        product.classList.add('product')
         let category  = document.createElement('div')
         category.classList.add('category')
         let productName = document.createElement('div')
@@ -73,13 +85,12 @@ async function getCategory(index,categories,sources){
         let imageSrc  = document.createElement('div')
         imageSrc.classList.add('imageSrc')
         let productPrice  = document.createElement('div')
-        productPrice.classList.add('price')
+        productPrice.classList.add('productPrice')
 
 
         for(let index in keys){
             let sourceList = sources.sources
             let source = sourceList[i][keys[index]]
-            console.log(category)
             
             switch(keys[index]){
                 case "category":
@@ -91,8 +102,8 @@ async function getCategory(index,categories,sources){
                 case "imageSrc":
                     let img = document.createElement('img'); 
                     img.src =  source
-                    img.style['maxWidth'] = '100px'
-                    img.style['maxHeight'] = '100px'
+                    img.style['maxWidth'] = '150px'
+                    img.style['maxHeight'] = '150px'
                     
                     imageSrc.append(img)
                     break
@@ -102,13 +113,13 @@ async function getCategory(index,categories,sources){
             }
         }
 
-        container.appendChild(category)
-        container.appendChild(productName)
-        container.appendChild(imageSrc)
-        container.appendChild(productPrice)
+        product.appendChild(category)
+        product.appendChild(productName)
+        product.appendChild(imageSrc)
+        product.appendChild(productPrice)
         //카테고리 분류하고 category 키에 배열을 값으로 추가. 배열안에 i값을 저장(삭제할 때 필요)
 
-        return container
+        return product
     }
 
 
